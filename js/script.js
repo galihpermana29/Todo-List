cc38a70d89f1d1f5ef12ae597643cc4266cd24cd
galihpermana29 <galihcbn123@gmail.com>
2020-10-11 02:40:12 +0700

    home, profile, about, addtask selesai. tinggal manifest dan sw aja

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.sidenav');
-	M.Sidenav.init(elems);
+   M.Sidenav.init(elems);
+   var dateEl = document.querySelectorAll('.datepicker')
+   M.Datepicker.init(dateEl)
+   var timeEl = document.querySelectorAll('.timepicker')
+   M.Timepicker.init(timeEl)
 });
